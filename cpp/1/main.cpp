1af01d0c6f29af93d27eec3e043ef01cea1c3521
[ilayda aytaş] <[aytasilayda49@gmail.com]>
2023-12-22 22:03:32 +0300

    cpp örnek kodları

---
+++ cpp/1/main.cpp
@@ -0,0 +1,79 @@
+//ekrana yazý basma
+#include <iostream>
+using namespace std;
+/*
+int main(int argc, char** argv) {
+	cout<<("Helloo");
+	return 0;
+}
+*/
+// ekrana 10 a kadar sayýlarý yazdýrma
+/*
+int main(){
+	for(int i=1;i<=10;i++){
+		cout<<(i);
+	}
+	return 0;
+}
+*/
+//ekrana yazmak için sayý adedini kullanýcýdan alma
+/*
+int main(){
+	cout<<("sayi adetini gir=");
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cout<<(i);
+		cout<<(" ");
+	}
+	return 0;
+}
+*/
+//ekrana * kullanarak basit üçken  sola dayalý
+/*
+int main(){
+	cout<<("ne kadar uzun bir ucgen olsun=");
+	int n;
+	cin>>n;
+	
+	for(int i=1;i<=n;i++){
+		for(int j=0;j<i;j++){
+			cout<<("*");
+		}
+		cout<<endl;
+	}
+	return 0;
+}*/
+//sayýlarla basit  bir üçken oluþtur sola yatýk
+/*
+int main(){
+	cout<<("kac basamak olsun");
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=0;j<i;j++){
+		cout<<(i);
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+*/
+//sayýlarla sola yatýk ters üçken oluþturma
+/*
+int main(){
+	cout<<("kac basamak olsun =");
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=n-i;j>=0;j--){
+			cout<<(" ");
+			cout<<(i);
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+*/
+//sayýlarla saða yatýk üçken oluþturma                 ??????????????????????????????????????????????????????????????????
+
