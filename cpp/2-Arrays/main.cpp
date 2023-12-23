becff2b241d98217c23798f0c2604123e64272c4
[ilayda aytaş] <[aytasilayda49@gmail.com]>
2023-12-23 18:33:20 +0300

    .

--- cpp/2-Arrays/main.cpp
+++ cpp/2-Arrays/main.cpp
@@ -1,25 +1,38 @@
-#include <iostream>
-//Write a program to reverse an array or string
-void revereseArray (int array[],int start,int end){
-	while(start<end){
-		int temp=array[start];
-		array[start]=array[end];
-		start=temp;
-		start++;
-		end--;
-	}
-void printArray(int arr[],int size){
-	for(int i=0;i<size;i++){
-		cout<<arr[i]<<" ";
-		cout<<endln;
-	}	
+#include<iostream>
+using namespace std;
+
+void most_occurred_number(int nums[], int size)
+{
+  int max_count = 0;
+  cout << "\nMost occurred number: ";
+  for (int i=0; i<size; i++)
+  {
+   int count=1;
+   for (int j=i+1;j<size;j++)
+       if (nums[i]==nums[j])
+           count++;
+   if (count>max_count)
+      max_count = count;
+  }
+
+  for (int i=0;i<size;i++)
+  {
+   int count=1;
+   for (int j=i+1;j<size;j++)
+       if (nums[i]==nums[j])
+           count++;
+   if (count==max_count)
+       cout << nums[i] << endl;
+  }
+ }
+ 
+int main()
+{
+    int nums[] = {4, 5, 9, 12, 9, 22, 45, 7};
+    int n = sizeof(nums)/sizeof(nums[0]);
+    cout << "Original array: ";
+    for (int i=0; i < n; i++) 
+    cout << nums[i] <<" ";
+    most_occurred_number(nums, n);
 }
-}
-int main(int argc, char** argv) {
-	int arr[]={1,2,3,4,5};
-	int n= sizeof(arr);
-	revereseArray(arr,0,n-1);
-	print(arr,n);
-	
-	return 0;
-}
+
